c11268c2a6e9f26a48c8deda25fb1f322ee11b6a
meir lejzerowicz <meirlejzerowicz@gmail.com>
2024-04-01 13:09:25 +0300

    better hyperparam milestone

--- hyperparams.py
+++ hyperparams.py
@@ -7,24 +7,24 @@ class Hyperparameters:
     INIT_LR: float = 1e-2
     BATCH_SIZE: int = 32
     EPOCHS: int = 100
-    MILESTONES = [10,100]
+    MILESTONES = [10,50]
 
     # datasets hyperparameters
     TRAIN_SPLIT: float = 0.75
     VAL_SPLIT: float = 1 - TRAIN_SPLIT
     
     # model hyperparameters
-    HIDDEN_SIZE: int = 16
+    HIDDEN_SIZE: int = 64
     NUM_LAYERS: int = 2
     N_INPUT_FEATURES: int = 2 # 3D input
     N_FUTURE_STEPS: int = 10
     SEQUENCE_LENGTH: int = 50
     TEACHER_FORCING: bool = True
-    TEACHER_FORCING_RATIO: float = 0.4
+    TEACHER_FORCING_RATIO: float = 0.7
 
     # simulator parameters
     dt = 20 # seconds. Time step
     T = 60*60 # seconds. Period of the orbit
     n = 2*np.pi/T # rad/s. Orbital rate
-    N_TRAJ = 10 # Number of trajectories
+    N_TRAJ = 400 # Number of trajectories
     max_t = T # seconds. Maximum time to simulate
